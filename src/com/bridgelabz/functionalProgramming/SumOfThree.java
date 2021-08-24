6e9e386537d918ca5bdfdb8e8775e14d8530168c
ajaybarath <ajaybarath3@gmail.com>
2021-08-24 14:37:03 +0530

    ADDED: count of sum of three numbers adds to 0

--- src/com/bridgelabz/functionalProgramming/SumOfThree.java
+++ src/com/bridgelabz/functionalProgramming/SumOfThree.java
@@ -1,5 +1,41 @@
 package com.bridgelabz.functionalProgramming;
 
+import java.util.Scanner;
+
 public class SumOfThree {
+	
+	public static void main(String args[]) {
+
+		int number = 0;
+		
+		Scanner s = new Scanner(System.in);
+		
+		number = s.nextInt();
+		
+		int count = 0;
+		
+		int array[] = new int [number];
+		
+		for (int i=0;i<number;i++) {
+			array[i] = s.nextInt();
+		}
+		
+		for (int i=0;i<number;i++) {
+			for (int j=i+1;j<number;j++) {
+				for (int k=j+1;k<number;k++) {
+					int sum = array[i] + array[j] + array[k];
+					
+					if(sum == 0) {
+						count ++;
+						System.out.println(array[i] + "," + array[j] + "," + array[k]);
+					}
+					
+				}
+			}
+		}
+		
+		System.out.println("Total occurence of triplets is " + count);
+		
+	}
 
 }
